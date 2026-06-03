af0c69d01a257b0befe0838cc19c39c8d629a5b9
emperialll <17363660+emperialll@users.noreply.github.com>
2026-06-03 23:28:19 +0200

    Implements the main search component with streamlit for frontend

--- app.py
+++ app.py
@@ -6,3 +6,17 @@ st.title("Shahriar Movie Recommander")
 # Have multiple tabs, each one for different features (search / movie details / recommendations)
 search_tab, movie_tab, rec_tab = st.tabs(["Search", "Movie details", "Recommend"])
 
+with search_tab:
+
+    st.header("Search movie")
+    query_string = st.text_input(label="Search for a movie")
+
+    srch_col1, srch_col2 = st.columns(2)
+    with srch_col1:
+        search_type = st.radio(
+            label="How do you want to search?",
+            options=["Vector", "Hybrid"]
+        )
+
+    with srch_col2:
+        value_range = st.slider(label="Rating range", value=(0.0, 5.0), step=0.1)
